984efacb9be46b18bdab44822097cc5cc53f3295
ingalls <nick@ingalls.ca>
2023-02-03 10:13:11 -0700

    Add dist

---
+++ dist/index.js
@@ -0,0 +1,40 @@
+/**
+ * @class
+ */
+export default class PublicError extends Error {
+    status;
+    safe;
+    constructor(status, err, safe, print = true) {
+        // Wrap postgres errors to ensure stack trace (line nums) are returned
+        if (err.hasOwnProperty('severity'))
+            err = new Error(err.message);
+        super(err ? err.message : safe);
+        if (print && ![400, 401, 402, 403, 404].includes(status))
+            console.error(err ? err : 'Error: ' + safe);
+        this.status = status;
+        this.safe = safe;
+    }
+    static respond(err, res, messages = []) {
+        if (err instanceof Error && (err.hasOwnProperty('status') || err.hasOwnProperty('statusCode'))) {
+            const serr = err;
+            err = new PublicError(serr.status || serr.statusCode, serr);
+        }
+        if (err instanceof PublicError) {
+            res.status(err.status).send({
+                status: err.status,
+                message: err.safe,
+                messages
+            });
+            // Handle generic errors
+        }
+        else {
+            console.error(err);
+            res.status(500).send({
+                status: 500,
+                message: 'Internal Server Error',
+                messages
+            });
+        }
+    }
+}
+//# sourceMappingURL=index.js.map
