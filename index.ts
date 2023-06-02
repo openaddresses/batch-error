644feeddb3b40361ca1812ebcc8aa9fb67cae62e
ingalls <nick@ingalls.ca>
2023-06-02 07:25:49 -0600

    Fix a few small type edge cases

--- index.ts
+++ index.ts
@@ -21,13 +21,13 @@ export default class PublicError extends Error {
         if (print && ![400, 401, 402, 403, 404].includes(status)) console.error(err ? err : 'Error: ' + safe);
 
         this.status = status;
-        this.safe = safe;
+        this.safe = safe || 'Generic Error';
     }
 
     static respond(err: unknown, res: Response, messages: object[] = []) {
         if (!(err instanceof PublicError) && err instanceof Error && (Object.hasOwn(err, 'status') || Object.hasOwn(err, 'statusCode'))) {
             const serr = err as StatusError;
-            err = new PublicError(serr.status || serr.statusCode, serr);
+            err = new PublicError(serr.status || serr.statusCode || 500, serr);
         }
 
         if (err instanceof PublicError) {
